710cc054900078f920d90808735850502826ec1b
banafshehkarimian <banafshe.karimian@gmail.com>
2024-10-27 20:41:58 -0400

    multi modal extending

--- utils/tracing_decorator.py
+++
@@ -1,9 +0,0 @@
-from torch.profiler import record_function
-
-def tracing_decorator(name):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            with record_function(name):
-                return func(*args, **kwargs)
-        return wrapper
-    return decorator
